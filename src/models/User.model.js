2485c81d267ff3bb6fbe5852a3a4dba4172508eb
Agathe Lurquin <agathe.lurquin@essca.eu>
2024-06-18 12:16:05 +0200

    (models) Password security

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -11,11 +12,17 @@ const userSchema = new Schema(
       type: String,
       required: [true, "The email is required."],
       unique: true,
-      match: [/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/, "Please use a valid email address."],
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+        "Please use a valid email address.",
+      ],
     },
     password: {
       type: String,
-      match: [/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/, 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter'],
+      match: [
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter",
+      ],
       required: true,
     },
     avatar: {
@@ -35,6 +42,22 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) return next;
+  try {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (passwordInput) {
+  return bcrypt.compare(passwordInput, this.password);
+};
 
 const User = model("User", userSchema);
 module.exports = User;
